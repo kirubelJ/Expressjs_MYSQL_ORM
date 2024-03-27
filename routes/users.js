45dfb8ac557afd58d98c03ba9fd0341bde4887cd
kirubel <kirubeljalleta62@gmail.com>
2024-03-27 15:40:47 +0300

    update and delete added

--- routes/users.js
+++ routes/users.js
@@ -17,4 +17,65 @@ router.post("/user", async (req, res) => {
   }
 });
 
+// Find all user , SELECT * FROM Table Name
+router.get("/users", async (req, res) => {
+  try {
+    const Users = await User.findAll();
+    res.status(200).json({ msg: "All users", data: Users }); //json array format
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to to get all users.", error: err });
+  }
+});
+
+// Find all user by attribute, like col name in this case id and first name
+router.get("/users/attribute", async (req, res) => {
+  try {
+    const Users = await User.findAll({
+      attributes: ["id", "firstName"],
+    });
+    res.status(200).json({ msg: "All users", data: Users }); //json array format
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to to get all users.", error: err });
+  }
+});
+
+// Update user last name by id
+router.put("/users/:id", async (req, res) => {
+  try {
+    const UpdatedUser = await User.update(
+      { lastName: req.body.updatedLastName },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json({ msg: "User data updated", data: UpdatedUser }); //json array format
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to to get all users.", error: err });
+  }
+});
+
+// Delete user by id
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const UpdatedUser = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ msg: "User data deleted", data: UpdatedUser }); //json array format
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to to get all users.", error: err });
+  }
+});
+
 module.exports = router;
